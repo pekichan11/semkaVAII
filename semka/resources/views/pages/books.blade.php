4cb4a3718f884110b6afddeec55a5306b8c971fb
Samuel Pekar <samuel.pekar@m2ms.sk>
2022-11-19 09:55:54 +0100

    listing all books into main

--- semka/resources/views/pages/books.blade.php
+++ semka/resources/views/pages/books.blade.php
@@ -1,1 +1,17 @@
-    
+@extends('master')
+
+@section('title', 'Listing all books')
+
+@section('main')
+    @foreach ($books as $book)
+    <ul>
+        <li>ID: {{ $book['id'] }}</li>
+        <li>Title: {{$book['title']}}</li>
+        <li>Plot: {{$book['plot']}}</li>
+    </ul>
+    @endforeach
+@stop
+
+@section('footer')
+    <h1>footer</h1>
+@stop
