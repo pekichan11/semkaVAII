9c8325070fcd2a67a4b71408678dcf6148aa4523
Samuel Pekar <samuel.pekar@m2ms.sk>
2022-11-19 13:49:10 +0100

    deleting book adding book

--- semka/app/Http/Controllers/BookController.php
+++ semka/app/Http/Controllers/BookController.php
@@ -4,17 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BookController extends Controller
 {
     //
     public function show($id) {
-        return Book::find($id);
+        return Book::findOrFail($id);
     }
 
     public function getAllBooks() {
         $books = Book::all();
         
-        return view('books', ['books' => $books]);
+        return view('pages/books', ['books' => $books]);
     }
+
+    public function deleteBook($id) {
+        
+        try {
+            $book = Book::findOrFail($id);
+        } catch(ModelNotFoundException $e) {
+            $books = Book::all();
+            return view('pages/books', ['books' => $books])->with('warning', 'Something went wrong');
+        }
+        $book->delete();
+        $books = Book::all();
+        return view('pages/books', ['books' => $books])->with('info', 'book has been deleted');
+    }
+
+    public function addBook(Request $request) {
+       $book = new Book();
+       $book->title = $request->get('title');
+       $book->plot = $request->get('plot');
+       $book->save();
+       
+        $books = Book::all();
+       return view('pages/books', ['books' => $books])->with('success','new book motherfucker');
+    }
+
 }
