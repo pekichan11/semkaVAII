84d2d8723acf905d1aa322f4a2483c5e5c21020f
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-05 20:56:09 +0100

    link to welcome page

--- semka/resources/views/include/navbar.blade.php
+++ semka/resources/views/include/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark first-navbar">
-    <a href="#" class="logo" >
+    <a href="/" class="logo" >
         <img src="{{ asset('/img/logo.png')}}" alt="logo" class="logo-img">
     </a>
     <div class="float-right">
