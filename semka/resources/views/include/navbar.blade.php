43c14e0bc174ed2d0ade6f2509a48fb023c1da01
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-05 20:51:28 +0100

    add route to nav bar

--- semka/resources/views/include/navbar.blade.php
+++ semka/resources/views/include/navbar.blade.php
@@ -5,16 +5,22 @@
     <div class="float-right">
     @guest
         <a href="{{ route('register') }}" class="nav-right-link" >Register</a>
+        <a href="{{ route('login') }}" class="nav-right-link">Login</a>
     @endguest
     @auth
         <a href="#" class="nav-right-link" >Profil</a>
+        <a href="#" class="nav-right-link" >Logout</a>
     @endauth
     </div>
    
 </nav>
+
 @auth
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark second-navbar">
         <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Domov</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Knihy</a>
             </li>
