50d8a048efcdb422cccea6d5e1697346f3a749e2
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-12 23:03:26 +0100

    add links

--- semka/resources/views/include/navbar.blade.php
+++ semka/resources/views/include/navbar.blade.php
@@ -19,16 +19,16 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark second-navbar">
         <ul class="navbar-nav me-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#">Domov</a>
+                <a class="nav-link" href=" {{ route("welcome") }}">Domov</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Knihy</a>
+                <a class="nav-link" href="/book">Knihy</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Požičané</a>
+                <a class="nav-link" href="/pozicky">Požičané</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Pokuty</a>
+                <a class="nav-link" href="/pokutky">Pokuty</a>
             </li>
         </ul>
     </nav>
