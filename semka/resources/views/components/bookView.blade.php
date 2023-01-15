d553f57ed8ff78abe5d9c0a76b6d5425a2e86d3f
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 10:37:32 +0100

    add class to div  img to own div

--- semka/resources/views/components/bookView.blade.php
+++ semka/resources/views/components/bookView.blade.php
@@ -1,9 +1,10 @@
 <?php 
     $random = rand(1, 300);
 ?>
-<div class="">
-
-    <img src="https://picsum.photos/id/<?= $random ?>/200/300" alt="">
+<div class="bookView">
+    <div class="img">
+        <img src="https://picsum.photos/id/<?= $random ?>/100/200" alt="">
+    </div>
     <h3>{{ $book['title'] }}</h3>
     <p>{{ $book['plot'] }}</p>
     <a href="#" class="btn btn-info">viac</a>
