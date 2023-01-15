2052f062fe1ba101157d809fbb1165983b3baba0
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 18:39:21 +0100

    comment structure

--- semka/app/Models/comment.php
+++ semka/app/Models/comment.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'text',
+    ];
+
+    public function user() {
+        return $this->hasOne(User::class);
+    }
+    
+    public function book() {
+        return $this->hasOne(Book::class);
+    }
 }
