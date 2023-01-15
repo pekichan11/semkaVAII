41a184b63933cae78d387049c6cc2574001c373e
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 17:03:25 +0100

    remove unused method

--- semka/app/Http/Controllers/UserController.php
+++ semka/app/Http/Controllers/UserController.php
@@ -7,24 +7,6 @@
 
     class UserController extends Controller
     {
-        public function store(Request $request) {
-            $request->validate([
-                'name' => ['required', 'max:255'],
-                'email' => ['required', 'unique:users', 'max:255', 'email'],
-                'password' => ['required', 'confirmed', 'max:255'],
-            ]);
-            
-            
-            $user = new User();
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->password = $request->password;
-            return $user;
-            $user->save();
-            event(new Registered($user));
-            Auth::login($user);
-            return view('/books');
-        }
-
+       //
     }
 ?>
