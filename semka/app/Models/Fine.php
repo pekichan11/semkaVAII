00f53bd1ca247fa734a4491779b69260a9e89952
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 17:24:19 +0100

    add atributes to fine

--- semka/app/Models/Fine.php
+++ semka/app/Models/Fine.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Fine extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'value'
+    ];
+
+    public function user() {
+        return $this->hasOne(Used::class);
+    }
 }
