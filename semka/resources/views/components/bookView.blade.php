87807e88681e4e2b640a0caa7d24f2e356abfe99
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 12:20:12 +0100

    change lenght of showing plot

--- semka/resources/views/components/bookView.blade.php
+++ semka/resources/views/components/bookView.blade.php
@@ -3,10 +3,19 @@
 ?>
 <div class="bookView">
     <div class="img">
-        <img src="https://picsum.photos/id/<?= $random ?>/100/200" alt="">
+        <img src="https://picsum.photos/id/<?= $random ?>/100/150" alt="">
     </div>
     <h3>{{ $book['title'] }}</h3>
-    <p>{{ $book['plot'] }}</p>
+    <p><?php 
+        if (strlen($book->plot) > 20) {
+            $text = substr($book['plot'], 0, 20);
+            $text = $text . ' ...';
+        } else {
+            $text = $book['plot'];
+        }
+        echo $text;
+        
+    ?></p>
     <a href="#" class="btn btn-info">viac</a>
     <a href="#" class="btn btn-success">objednaj</a>
 </div>
