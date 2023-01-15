f76916fa144b4b35ec2491961f922e26fc12e1dd
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 21:52:26 +0100

    get all loans for useer

--- semka/app/Http/Controllers/LoanController.php
+++ semka/app/Http/Controllers/LoanController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Loan;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoanController extends Controller
 {
-    //
+    public function getAll() {
+        $user_id = Auth::user()->id;
+        $loans = Loan::where('user_id', $user_id)->get();
+        return response()->json($loans);
+    }
 }
