10ceacd72e52459643831d9005aaefe0a55cc75a
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-15 18:11:11 +0100

    hidden password

--- semka/app/Models/User.php
+++ semka/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -19,10 +18,10 @@ class User extends Authenticatable
     ];
 
     protected $hidden = [
-        'remember_token',
+        'password',
     ];
+    
 
-   
     public function setPasswordAttribute($password) {
         $this->attributes['password'] = bcrypt($password);
     }
