76112193f7616db0c860a550c109083b7730dfae
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-20 19:17:51 +0100

    get book with books comments

--- semka/app/Http/Controllers/BookController.php
+++ semka/app/Http/Controllers/BookController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BookController extends Controller
 {
-    //
     public function show($id) {
-        return Book::findOrFail($id);
+        $comments = Comment::where('book_id', $id)->get();
+
+        $book = Book::findOrFail($id);
+        return view('pages.oneBook',['book' => $book, 'comments' => $comments]);
+        return response()->json([$comments, $book]);
     }
 
     public function getAllBooks() {
