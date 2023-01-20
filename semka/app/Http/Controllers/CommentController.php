0983aa0dc7005f41ca56b9551460612e30ff41f4
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-20 19:18:09 +0100

    sotre comment, get all commets by book id

--- semka/app/Http/Controllers/CommentController.php
+++ semka/app/Http/Controllers/CommentController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $comment = new Comment();
+        $comment->text = $request->get('text');
+        $comment->user_id = $request->get('user_id');
+        $comment->book_id = $request->get('book_id');
+        $comment->save();
+        return redirect('/book' .  '/' .$request->get('book_id'));
+    }
+
+    public function getCommetsByBookId($id) {
+        return Comment::where('book_id', $id)->get();
+    }
 }
