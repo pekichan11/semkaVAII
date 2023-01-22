a1c4941387d74a845f4a9196a3f4047ecbc51fb0
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-22 12:50:05 +0100

    get all comments from database

--- semka/resources/views/components/coments.blade.php
+++ semka/resources/views/components/coments.blade.php
@@ -1,4 +1,22 @@
-<form method="POST" action="/newcomment">
+<?php 
+    use App\Models\User;
+?>
+<div class="comments">
+    @foreach ($comments as $item)
+    <?php 
+        $user = User::findOrFail($item->user_id);
+    ?>
+    <div class="card border-info mb-3 comment">
+        <div class="card-header">{{ date("m.d H:i",strtotime($item['created_at'])) }}</div>
+        <div class="card-body">
+            <p class="card-text"><?= $user->name ?></p>
+            <h4 class="card-title">{{ $item['text'] }}</h4>
+        </div>
+    </div>
+    @endforeach
+</div>
+
+<form method="POST" action="{{ route('comment') }}">
     @csrf
     <input type="hidden" name="book_id" value="{{ $book['id'] }}">
     <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
