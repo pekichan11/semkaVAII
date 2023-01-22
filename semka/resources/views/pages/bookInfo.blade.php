2199480a53c1f935cefad5342a616f1130a2bb6c
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-22 12:50:24 +0100

    refactor oneBook page

---
+++ semka/resources/views/pages/bookInfo.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+
+@section('title', $book->title)
+
+@section('main')
+
+    <div class="book-info">
+        <img class="book-info-img" src="{{ $book['img'] }}" alt="">
+        <div class="book-info-content">
+            <h2>{{ $book['title'] }}</h2>
+            <p>{{ $book['plot'] }}</p>
+        </div>
+    </div>
+    @include('components\coments')
+@endsection   
+
+@section('footer')
+    
+@endsection
+
+
+
+    
