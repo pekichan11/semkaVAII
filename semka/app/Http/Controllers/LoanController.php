8c4e7085e73fcb3a20843e9eebb2d84929ebd186
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-22 14:38:36 +0100

    return view

--- semka/app/Http/Controllers/LoanController.php
+++ semka/app/Http/Controllers/LoanController.php
@@ -12,6 +12,6 @@ class LoanController extends Controller
     public function getAll() {
         $user_id = Auth::user()->id;
         $loans = Loan::where('user_id', $user_id)->get();
-        return response()->json($loans);
+        return view('pages.pozicane', ['loans' => $loans]);
     }
 }
