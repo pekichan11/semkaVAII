70183024c220e816f1f7381469c5bccaaa8f4520
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-05 13:30:45 +0100

    path to img

--- semka/resources/views/pages/bookInfo.blade.php
+++ semka/resources/views/pages/bookInfo.blade.php
@@ -5,7 +5,7 @@
 @section('main')
 
     <div class="book-info">
-        <img class="book-info-img" src="{{ $book['img'] }}" alt="">
+        <img class="book-info-img" src="{{ asset('img/books')."/".$book['img'] }}" alt="">
         <div class="book-info-content">
             <h2>{{ $book['title'] }}</h2>
             <p>{{ $book['plot'] }}</p>
