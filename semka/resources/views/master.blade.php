21d1930bcc69e8e9e855c770c258c9da9dad787a
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-05 16:46:14 +0100

    script for dropdown

--- semka/resources/views/master.blade.php
+++ semka/resources/views/master.blade.php
@@ -10,9 +10,9 @@
     <link rel="icon"  href="{{ asset('favicon.ico')}}">
 </head>
 <body>
-    @include('include.flash-message')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    @include('include.flash-message')
     @if(Request::segment(1) != 'login' && Request::segment(1) != 'register')
         <header>
             @include('include.navbar')
@@ -29,8 +29,6 @@
             @yield('footer')
         </div>
     </footer>
-
-
     <script src="{{ asset('/js/app.js')}}"></script>
 </body>
 </html>
