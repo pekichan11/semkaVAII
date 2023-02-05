510ad33255b7760af68c3e36966e4d8825766c50
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-05 16:46:25 +0100

    new navbar

--- semka/resources/views/include/navbar.blade.php
+++ semka/resources/views/include/navbar.blade.php
@@ -16,20 +16,30 @@
 </nav>
 
 @auth
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark second-navbar">
-        <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-                <a class="nav-link" href=" {{ route("welcome") }}">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/book">Knihy</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/pozicky">Požičané</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/pokutky">Pokuty</a>
-            </li>
-        </ul>
-    </nav>
+    
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href=" {{ route("welcome") }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/book">Knihy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/pozicky">Požičané</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/pokutky">Pokuty</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      
 @endauth
