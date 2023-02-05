b423e8f2fc5b702f7d105dbd38ef2cce0db7b20b
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-05 10:48:08 +0100

    refactor view

--- semka/resources/views/pages/pozicane.blade.php
+++ semka/resources/views/pages/pozicane.blade.php
@@ -20,19 +20,14 @@
                 @for ($i = 0; $i < 4; $i++)
                     @if ($i % 2 == 0)
                     <tr class="table-active">
-                        <th scope="row">{{ $i + 1 }}</th>
-                        <td>Column content</td>
-                        <td>Column content</td>
-                        <td>Column content</td>
-                    </tr>
                     @else
                     <tr>
+                    @endif
                         <th scope="row">{{ $i + 1 }}</th>
                         <td>Column content</td>
                         <td>Column content</td>
                         <td>Column content</td>
                     </tr>
-                    @endif
                 @endfor
             </tbody>
         </table>
