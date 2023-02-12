5d0e98bb0c05da2ab0bebfbbe32cf4a104e0c5ec
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-12 20:38:42 +0100

    refactor books info page

--- semka/resources/views/components/coments.blade.php
+++ semka/resources/views/components/coments.blade.php
@@ -1,26 +1,37 @@
 <?php 
     use App\Models\User;
-?>
-<div class="comments">
-    @foreach ($comments as $item)
-    <?php 
-        $user = User::findOrFail($item->user_id);
-    ?>
-    <div class="card border-info mb-3 comment">
-        <div class="card-header">{{ date("m.d H:i",strtotime($item['created_at'])) }}</div>
-        <div class="card-body">
-            <p class="card-text"><?= $user->name ?></p>
-            <h4 class="card-title">{{ $item['text'] }}</h4>
-        </div>
-    </div>
-    @endforeach
-</div>
 
-<form method="POST" action="{{ route('comment') }}">
+    $int = intval(count($comments));
+?>
+
+<form method="POST" class="form-control dark" action="{{ route('comment') }}">
     @csrf
+    <input type="hidden" name="user_name" value="{{Auth::user()->name }}">
     <input type="hidden" name="book_id" value="{{ $book['id'] }}">
     <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-    <input type="text" name="text" required>
+    <input type="text" name="text" class="form-control mb-3" required>
 
-    <button type="submit" class="btn btn-danger" id="comment">submit</button>
+    <button type="submit" class="btn " id="comment">submit</button>
 </form>
+
+<div class="comments">
+    @for ($i = intval(count($comments)) - 1; $i > intval(count($comments) - 4) && $i >= 0; $i--)
+    <?php $user = User::findOrFail($comments[$i]->user_id);?>
+    <div class="card border-info mb-3 comment">
+        <div class="card-header">{{ date("m.d H:i",strtotime($comments[$i]['created_at'])) }}</div>
+        <div class="card-body">
+            <p class="card-text"><?= $user->name ?></p>
+            <h4 class="card-title">{{ $comments[$i]['text'] }}</h4>
+        </div>
+    </div>
+    @endfor
+    
+    <ul class="comment-pages">
+        @for ($j = 0; $j < ceil(intval(count($comments)) / 3); $j++)
+            <li class="comment-page">
+                <a href="#" class="comment-page-link"><?= $j + 1 ?></a>
+            </li>    
+        @endfor
+        </ul>
+</div>
+
