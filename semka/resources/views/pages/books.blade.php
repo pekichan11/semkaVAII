e660a84ed8cd26ec8eb244de9a01378d34d5295b
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-12 13:18:01 +0100

    remove blank spaces

--- semka/resources/views/pages/books.blade.php
+++ semka/resources/views/pages/books.blade.php
@@ -3,20 +3,14 @@
 @section('title', 'Listing all books')
 
 @section('main')
-
     @if(isset($books))
         @foreach ($books as $book)
-        
-            @include('components.bookView')
-        
+                    @include('components.bookView')
         @endforeach
     @endif
-
 @stop
 
 @section('footer')
-
     <h1>footer</h1>
     <a href="/add" class="btn btn-success"> add new</a>
-
 @stop
