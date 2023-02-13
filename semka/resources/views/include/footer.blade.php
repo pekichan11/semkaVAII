f6c712534b99b671a878897fddb7b0d46c4ec94d
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 15:03:05 +0100

    footer

---
+++ semka/resources/views/include/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <a class="footer-logo" href="/">
+        <img src="{{asset('img/logo.png')}}" alt="" >
+    </a>
+    <ul>
+        <li><a href="#"><img src="{{asset('img/ig.png')}}" alt="">Instagram</a></li>
+        <li><a href="#"><img src="{{asset('img/fb.png')}}" alt="">Facebook</a></li>
+        <li> <a href="mailto:info@e-kniznica.com"><img src="{{asset('img/obalka.png')}}" alt="">info@e-kniznica.com</a></li>
+    </ul>
+</footer>
