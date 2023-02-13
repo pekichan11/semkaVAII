98b59e30699e0341879c41f5478fb8a47b9d3063
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 18:44:59 +0100

    only autor or admin can play with coments

--- semka/app/Http/Controllers/CommentController.php
+++ semka/app/Http/Controllers/CommentController.php
@@ -27,33 +27,33 @@ class CommentController extends Controller
 
     public function show($id) {
         $comment = Comment::findOrFail($id);
-        if(Auth::user()->id !== $comment->user_id) {
-            return aboirt(404);
+        if(Auth::user()->id === $comment->user_id || Auth::user()->role === 'admin') {
+            return view('pages.editComment', ['comment'=> $comment]);
         }
-        return view('pages.editComment', ['comment'=> $comment]);
+        return abort(404);
     }
 
     public function deleteComment($id) {
         $comment = Comment::findOrFail($id);
-        if(Auth::user()->id !== $comment->user_id) {
-            return aboirt(404);
+        if(Auth::user()->id === $comment->user_id || Auth::user()->role === 'admin') {
+            $book_id = $comment->book_id;
+            $comment->delete();
+            session(['info' => 'Comment deleted!']);
+            return redirect("/book" . "/" . $book_id);
         }
-        $book_id = $comment->book_id;
-        $comment->delete();
-        session(['info' => 'Comment deleted!']);
-        return redirect("/book" . "/" . $book_id);
+        return abort(403);
     }
 
     public function edit($id, Request $request) {
         $comment = Comment::findOrFail($id);
-        if(Auth::user()->id !== $comment->user_id) {
-            return aboirt(404);
+        if(Auth::user()->id === $comment->user_id || Auth::user()->role === 'admin') {
+            $comment->text = $request->get('text');
+            $comment->save();
+            $book_id = $comment->book_id;
+            session(['info' => 'Comment edited!']);
+            return redirect("/book" . "/" . $book_id);
         }
-        $comment->text = $request->get('text');
-        $comment->save();
-        $book_id = $comment->book_id;
-        session(['info' => 'Comment edited!']);
-        return redirect("/book" . "/" . $book_id);
+        return aboirt(403);
     }
 
 }
