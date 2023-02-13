7089432963fd6b304eae135564d2615cd3511bbd
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 12:38:29 +0100

    link for autor if the bokk has one

--- semka/resources/views/pages/bookInfo.blade.php
+++ semka/resources/views/pages/bookInfo.blade.php
@@ -7,8 +7,12 @@
         <a class="link-book-img" href="{{ asset('img/books')."/".$book['img'] }}">
             <img class="book-info-img" src="{{ asset('img/books')."/".$book['img'] }}" alt="">
         </a>
+        
         <div class="book-info-content">
             <h2>{{ $book['title'] }}</h2>
+            @if ($book['autor_id'] !== null)
+            <a href="/autor/{{$book['autor_id']}}">Autor</a>
+            @endif
             <p>{{ $book['plot'] }}</p>
         </div>
     </div>
