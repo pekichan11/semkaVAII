11f911974723458d2f4473344d289eb5e3ef2903
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 15:03:57 +0100

    comment crud

--- semka/app/Http/Controllers/CommentController.php
+++ semka/app/Http/Controllers/CommentController.php
@@ -25,10 +25,35 @@ class CommentController extends Controller
                         ->get();
     }
 
-    public function getThreeComments($id, $page) {
-        return $comments = Comment::where('book_id', $id)
-                            ->orderBy('updated_at', 'desc')
-                            ->limit(3 * $page)
-                            ->get();
+    public function show($id) {
+        $comment = Comment::findOrFail($id);
+        if(Auth::user()->id !== $comment->user_id) {
+            return aboirt(404);
+        }
+        return view('pages.editComment', ['comment'=> $comment]);
     }
+
+    public function deleteComment($id) {
+        $comment = Comment::findOrFail($id);
+        if(Auth::user()->id !== $comment->user_id) {
+            return aboirt(404);
+        }
+        $book_id = $comment->book_id;
+        $comment->delete();
+        session(['info' => 'Comment deleted!']);
+        return redirect("/book" . "/" . $book_id);
+    }
+
+    public function edit($id, Request $request) {
+        $comment = Comment::findOrFail($id);
+        if(Auth::user()->id !== $comment->user_id) {
+            return aboirt(404);
+        }
+        $comment->text = $request->get('text');
+        $comment->save();
+        $book_id = $comment->book_id;
+        session(['info' => 'Comment edited!']);
+        return redirect("/book" . "/" . $book_id);
+    }
+
 }
