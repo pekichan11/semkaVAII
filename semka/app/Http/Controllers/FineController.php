17df4486a09828a22e9aad1c0a6d01222e3d68cd
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 18:44:05 +0100

    controller for fines

--- semka/app/Http/Controllers/FineController.php
+++ semka/app/Http/Controllers/FineController.php
@@ -9,10 +9,42 @@ use Illuminate\Support\Facades\Auth;
 
 class FineController extends Controller
 {
+    public function store(Request $request) {
+        
+        if(Auth::user()->role !== 'admin') {
+            return abort(403);
+        }
+        $fine = new Fine();
+        $fine->user_id = $request->get('user_id');
+        $fine->value = $request->get('value');
+        $fine->text = $request->get('text');
+        $fine->save();
+        return redirect('/pozicky');
+    }
 
     public function show() {
-        $fines = Fine::where('user_id', Auth::user()->id)->get();
+        if(Auth::user()->role ==='admin') {
+            $fines = Fine::orderBy('user_id')->get();
+        } else {
+            $fines = Fine::where('user_id', Auth::user()->id)->get();
+        }
         return view('pages.fines', ['fines' => $fines]); 
+    }
+
+    public function formWithUserId($user_id) {
+        if(Auth::user()->role !== 'admin') {
+            return abort(401);
+        }
+        return view('pages.editFine', ['user_id' => $user_id]);
+    }
+
+    public function destroy($id) {
+        if(Auth::user()->role !== 'admin') {
+            return abort(401);
+        }
+        $fine = Fine::findOrFail($id);
+        $fine->delete();
+        return $fine;
     }
     
 }
