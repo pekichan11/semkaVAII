304b0116a475b6b911730f098d47f3e6124f5b42
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 12:38:15 +0100

    find autor with his books

--- semka/app/Http/Controllers/AutorController.php
+++ semka/app/Http/Controllers/AutorController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Autor;
+use App\Models\Book;
 
 class AutorController extends Controller
 {
     //
+
+    public function getAutorById($id) {
+        $autor = Autor::findOrFail($id);
+        $books = Book::where('autor_id', $id)->limit(4)->get();
+        return view('pages.autor', ['autor' => $autor, 'books' => $books]);
+    }
 }
