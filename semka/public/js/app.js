8f8bde408c6a77d7bd0dfb7de0be07cb4cb60731
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 11:42:18 +0100

    upratovanie refactor

--- semka/public/js/app.js
+++ semka/public/js/app.js
@@ -1,6 +1,5 @@
 (function($) {
     const smallX = $('.close');
-
     smallX.on('click', function(event) {
         event.preventDefault();
         smallX.parent().fadeOut(300);
@@ -15,14 +14,6 @@
             email.setCustomValidity("");
           }
      });
-
-    const link = $('.next-comment-link');
-    link.on('click',(e) => { 
-      e.preventDefault();
-      fetch('/comments/' + link.value)
-        .then((res) => res.json());
-        
-    })
 
     const bookLink = $('.link-book-img');
     bookLink.on('click',(e) => {
@@ -46,12 +37,12 @@
 
     $("#comment").on('click', (e) => {
       e.preventDefault();
+      console.log("nehe");
       const data = {
         user_id: $('input[name="user_id"]').val(),
         _token: $('input[name="_token"]').val(),
         book_id: $('input[name="book_id"]').val(),
         text: $('input[name="text"]').val(),
-        user_name: $('input[name="user_name"]').val(),
       };
       $.ajax({
         url: 'http://localhost:8000/addComment',
@@ -59,17 +50,12 @@
         data: data,
         success: (res) => {
           const date = new Date(res.updated_at);
-          const stringDate = String(date.getDate()).padStart(2, '0') + '.' +
-            String(date.getMonth() + 1).padStart(2,'0') + ' ' +
-            date.getHours() + ':' + date.getMinutes();
-          let comment = $('<div class="card border-info mb-3 comment"></div>');
-          let cardHeader = $('<div class="card-header">' + stringDate +  '</div>');
-          cardHeader.appendTo(comment);
-          let commentBody = $('<div class="card-body"></div>');
-          $('<p class="card-text">' + data.user_name + '</p>').appendTo(commentBody);
-          $('<h4 class="card-title">' + data.text + '</h4>').appendTo(commentBody);
-          commentBody.appendTo(comment);
-          
+          const dataForComment = {
+            date: date,
+            text: res.text,
+            user_name: res.user_name,
+          };
+          let comment = createComment(dataForComment);
           let komentik = $('.comment')[2];
           if (komentik) {
             komentik.remove();
@@ -81,9 +67,54 @@
 
     $('.comment-page-link').on('click', async (e) => {
       e.preventDefault();
-      const response = await fetch('url', {
-        method: 'GET',
-      });
-      const data = await response.json();
-    })
+      try {
+        const book_id = e.target.href.substring(27,28);
+        const response = await fetch(`http://localhost:8000/getComments/${book_id}`, {
+          headers: {
+            'Content-Type': "application/json",
+          },
+        });
+        const data = await response.json();
+        let comments = $('.comment');
+        for(let i = 0; i < comments.length; i++) {
+          comments[i].remove();
+        }
+        let komenty = [];
+        for (let index = e.target.text * 3 - 3; index < data.length; index++) {
+          const date = new Date(data[index].updated_at);
+            const dataForComment = {
+              date: date,
+              text: data[index].text,
+              user_name: data[index].user_name,
+            };
+            const comment = createComment(dataForComment);
+            komenty[index % 3] = comment;
+            
+            if(index == e.target.text * 3 - 1) {
+              break;
+            }
+          }
+          const komentare = $('.comments');
+          for (let index = komenty.length - 1; index >= 0; index--) {
+            komentare.prepend(komenty[index]);
+          }
+      } catch (err) { 
+        console.log(err);
+      } 
+    });
+
+
+    function createComment(data) {
+      const stringDate = String(data.date.getDate()).padStart(2, '0') + '.' +
+            String(data.date.getMonth() + 1).padStart(2,'0') + ' ' +
+            String(data.date.getHours()).padStart(2, '0') + ':' + String(data.date.getMinutes()).padStart(2, '0');
+      let comment = $('<div class="card border-info mb-3 comment"></div>');
+      let cardHeader = $('<div class="card-header">' + stringDate +  '</div>');
+      cardHeader.appendTo(comment);
+      let commentBody = $('<div class="card-body"></div>');
+      $('<p class="card-text">' + data.user_name + '</p>').appendTo(commentBody);
+      $('<h4 class="card-title">' + data.text + '</h4>').appendTo(commentBody);
+      commentBody.appendTo(comment);
+      return comment;
+    }
 }(jQuery))
