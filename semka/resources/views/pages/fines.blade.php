0e51a4a5f101465d5bb90f11cacf2b39d5cf91a6
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 18:42:07 +0100

    change admin view

--- semka/resources/views/pages/fines.blade.php
+++ semka/resources/views/pages/fines.blade.php
@@ -1,5 +1,7 @@
 @extends('master')
-
+<?php 
+    use Illuminate\Support\Facades\Auth; 
+?>
 @section('title', 'pokuty') 
 
 @section('main')
@@ -9,8 +11,12 @@
         @if (isset($fines))
             @foreach ($fines as $item)
             <div class="card border-info mb-3 comment">
-                <div class="card-header">{{ date("m.d H:i",strtotime($item->created_at)) }}</div>
+                <div class="card-header">{{ date("m.d H:i",strtotime($item->created_at)) }} </div>
                 <div class="card-body">
+                    <?php
+                        if (Auth::user()->role == 'admin')
+                            echo '<a id="delete-fine" href="'. $item->id .'" class="btn btn-danger float-right">remove</a>';
+                    ?>
                     <h4 class="card-title">{{ $item['value'] }}€</h4>
                     <p class="card-text">{{ $item['text'] }}</p>
                 </div>
