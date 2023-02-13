f8370d8315f07119a7d538b6dde252553dc78234
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 11:43:36 +0100

    fine show

--- semka/app/Http/Controllers/FineController.php
+++ semka/app/Http/Controllers/FineController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Fine;
+use Illuminate\Support\Facades\Auth;
+
 
 class FineController extends Controller
 {
+
+    public function show() {
+        $fines = Fine::where('user_id', Auth::user()->id)->get();
+        return view('pages.fines', ['fines' => $fines]); 
+    }
     
 }
