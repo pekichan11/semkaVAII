73ec3fcfa9859f75732582164e11141b70c1518e
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 18:58:55 +0100

    add autor link for admin

--- semka/resources/views/pages/autors.blade.php
+++ semka/resources/views/pages/autors.blade.php
@@ -4,6 +4,9 @@
 
 @section('main')
     <h1>Autory</h1>
+    @if (Auth::user()->role === 'admin')
+        <a href="addAutor" class="btn btn-success">add autor</a>
+    @endif
     <ul>
         @foreach ($autors as $item)
             <li><a href="/autor/{{$item['id']}}">{{$item['name']}}</a></li>
