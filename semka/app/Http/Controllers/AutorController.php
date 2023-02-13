3d53e9ce53b14c24ac1526e6d7950c93d7295fbf
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-02-13 18:58:43 +0100

    create autor

--- semka/app/Http/Controllers/AutorController.php
+++ semka/app/Http/Controllers/AutorController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 use App\Models\Autor;
@@ -8,7 +9,27 @@ use App\Models\Book;
 
 class AutorController extends Controller
 {
-    //
+    public function store(Request $request) {
+        if(Auth::user()->role !== 'admin') {
+            return abort(404);
+        }
+        $autor = new Autor();
+        $autor->name = $request->get('name');
+        $autor->birthdate = $request->get('birthdate');
+        if($request->file('img')) {
+            $file = $request->file('image');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('img/autors'), $filename);
+            $autor->img = $filename;
+        }
+        $autor->info = $request->get('info');
+        $autor->save();
+        return redirect('/autors');
+    }
+
+    public function getAll() {
+        return view('pages.autors', ['autors' => Autor::orderBy('name')->get()]);
+    }
 
     public function getAutorById($id) {
         $autor = Autor::findOrFail($id);
